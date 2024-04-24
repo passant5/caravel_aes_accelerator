46776228041791fadd7c9265806d5099f23a7669
Passant <bassant.hassan@efabless.com>
2024-04-24 20:32:46 +0200

    add implementation of AES user_project_wrapper

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -82,10 +82,10 @@ module user_project_wrapper #(
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-user_proj_example mprj (
+aes_wb_wrapper mprj (
 `ifdef USE_POWER_PINS
-	.vccd1(vccd1),	// User area 1 1.8V power
-	.vssd1(vssd1),	// User area 1 digital ground
+	.VPWR(vccd2),
+	.VGND(vssd2),
 `endif
 
     .wb_clk_i(wb_clk_i),
@@ -100,22 +100,7 @@ user_proj_example mprj (
     .wbs_adr_i(wbs_adr_i),
     .wbs_dat_i(wbs_dat_i),
     .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
-
-    // Logic Analyzer
-
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
-
-    // IO Pads
-
-    .io_in ({io_in[37:30],io_in[7:0]}),
-    .io_out({io_out[37:30],io_out[7:0]}),
-    .io_oeb({io_oeb[37:30],io_oeb[7:0]}),
-
-    // IRQ
-    .irq(user_irq)
+    .wbs_dat_o(wbs_dat_o)
 );
 
 endmodule	// user_project_wrapper
